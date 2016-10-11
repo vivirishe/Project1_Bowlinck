268220905b28e88484f0bfbea64e19822c28ca48
viviana rishe <vivianacuri@gmail.com>
2016-10-11 16:48:27 -0700

    adding ball movement and logic for hiting pins

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log('yup')
 
+var startResetButton = $('#startGame').text()
 //jQuery capturing Player's Name
 $('#enterName').on('click', function(){
   // e.preventDefault()
@@ -8,20 +9,38 @@ $('#enterName').on('click', function(){
   $('#nameField').val('')
 })
 //startGame button
-  $('#startGame').on('click', function(){
 
-    $('#startGame').text('Reset')
+  $('#startGame').on('click', function(){
+    if (startResetButton === "Start Game") {
+      $('#startGame').text('Reset')
+      startResetButton = $('#startGame').text()
       //variables for Pins
-     pin1 = true
-     pin2 = true
-     pin3 = true
-     pin4 = true
-     pin5 = true
-     pin6 = true
-     pin7 = true
-     pin8 = true
-     pin9 = true
-     pin10 = true
+      pin1 = true
+      pin2 = true
+      pin3 = true
+      pin4 = true
+      pin5 = true
+      pin6 = true
+      pin7 = true
+      pin8 = true
+      pin9 = true
+      pin10 = true
+
+    }else {
+      pin1 = true
+      pin2 = true
+      pin3 = true
+      pin4 = true
+      pin5 = true
+      pin6 = true
+      pin7 = true
+      pin8 = true
+      pin9 = true
+      pin10 = true
+
+       x = 245
+       y = 500
+    }
 
   })
 
@@ -29,56 +48,66 @@ $('#enterName').on('click', function(){
 var selectLevel = 0
 var shootsLeft = 0
 var score = 0
-//var startResetButton = $('#startGame').text()
 //setting number of shootsLeft and setting score to 0
 //how to compare with Start and Reset for not allowing to change level once tye game starts
-if ($('#startGame').text() === 'Start Game') {
+
   $('#easy').on('click', function(){
     //selectLevel = $('#easy').val(level).addClass('levelGameTracker')
     shootsLeft = 6
-    score = 0
     selectLevel = $('#easy').text()
-    if(($('#level').children().length > 1) && ($('#shoots').children().length > 1) && ($('#score').children().length > 1)){
-      $('#level').children().eq(1).html(selectLevel)
-      $('#shoots').children().eq(1).html(shootsLeft)
-      $('#score').children().eq(1).html(score)
-    }else{
-      $('<h3 />', {html: selectLevel}).appendTo('#level')
-      $('<h3 />', {html: shootsLeft}).appendTo('#shoots')
-      $('<h3 />', {html: score}).appendTo('#score')
+    if (startResetButton === "Start Game") {
+      if(($('#level').children().length > 1) && ($('#shoots').children().length > 1) && ($('#score').children().length > 1)){
+        $('#level').children().eq(1).html(selectLevel)
+        $('#shoots').children().eq(1).html(shootsLeft)
+        $('#score').children().eq(1).html(score)
+      }else{
+        $('<h3 />', {html: selectLevel}).appendTo('#level')
+        $('<h3 />', {html: shootsLeft}).appendTo('#shoots')
+        $('<h3 />', {html: score}).appendTo('#score')
+      }
+    }else {
+      alert('You already chose a level')
     }
   })
 
   $('#medium').on('click', function(){
-    score = 0
+
     shootsLeft = 5
     selectLevel = $('#medium').text()
-    if(($('#level').children().length > 1) && ($('#shoots').children().length > 1) && ($('#score').children().length > 1)){
-      $('#level').children().eq(1).html(selectLevel)
-      $('#shoots').children().eq(1).html(shootsLeft)
-      $('#score').children().eq(1).html(score)
-    }else{
-      $('<h3 />', {html: selectLevel}).appendTo('#level')
-      $('<h3>', {html: shootsLeft}).appendTo('#shoots')
-      $('<h3>', {html: score}).appendTo('#score')
+    if (startResetButton === "Start Game") {
+      if(($('#level').children().length > 1) && ($('#shoots').children().length > 1) && ($('#score').children().length > 1)){
+        $('#level').children().eq(1).html(selectLevel)
+        $('#shoots').children().eq(1).html(shootsLeft)
+        $('#score').children().eq(1).html(score)
+      }else{
+        $('<h3 />', {html: selectLevel}).appendTo('#level')
+        $('<h3>', {html: shootsLeft}).appendTo('#shoots')
+        $('<h3>', {html: score}).appendTo('#score')
+      }
+    }else {
+      alert('You already chose a level')
     }
   })
 
   $('#advanced').on('click', function(){
     shootsLeft = 4
-    score = 0
     selectLevel = $('#advanced').text()
-    if(($('#level').children().length > 1) && ($('#shoots').children().length > 1) && ($('#score').children().length > 1)){
-      $('#level').children().eq(1).html(selectLevel)
-      $('#shoots').children().eq(1).html(shootsLeft)
-      $('#score').children().eq(1).html(score)
-    }else{
-      $('<h3 />', {html: selectLevel}).appendTo('#level')
-      $('<h3>', {html: shootsLeft}).appendTo('#shoots')
-      $('<h3>', {html: score}).appendTo('#score')
+    if (startResetButton === "Start Game") {
+
+      if(($('#level').children().length > 1) && ($('#shoots').children().length > 1) && ($('#score').children().length > 1)){
+        $('#level').children().eq(1).html(selectLevel)
+        $('#shoots').children().eq(1).html(shootsLeft)
+        $('#score').children().eq(1).html(score)
+      }else{
+        $('<h3 />', {html: selectLevel}).appendTo('#level')
+        $('<h3>', {html: shootsLeft}).appendTo('#shoots')
+        $('<h3>', {html: score}).appendTo('#score')
+      }
+    }else {
+      alert('You already chose a level')
     }
   })
-}
+
 //=================================================
 
 //=================================================
@@ -100,40 +129,34 @@ if ($('#startGame').text() === 'Start Game') {
 //x = 0 && y = -2
 //value of y it will define the velocity of the ball
   var dx = 0
-  var dy = -1
+  var dy = -2
  //moving indicators (left and right)
   var rightPressed = false
   var leftPressed = false
   var upPressed = false
-var pin1
-var pin2
-var pin3
-var pin4
-var pin5
-var pin6
-var pin7
-var pin8
-var pin9
-var pin10
+  var pin1
+  var pin2
+  var pin3
+  var pin4
+  var pin5
+  var pin6
+  var pin7
+  var pin8
+  var pin9
+  var pin10
+  var bowlBall
 
   //Ball
   function drawBall(){
-    ctx.beginPath()
-    ctx.arc(x, y, radius, 0,2*Math.PI)
-    ctx.fillStyle = 'blue'
-    ctx.fill()
-    ctx.stroke()
-    ctx.closePath()
+      ctx.beginPath()
+      ctx.arc(x, y, radius, 0,2*Math.PI)
+      ctx.fillStyle = 'blue'
+      ctx.fill()
+      ctx.stroke()
+      ctx.closePath()
   }
 
-  //drawing Pins
-  //Study if I have time to redoit
-  // ctx.beginPath()
-  //     ctx.moveTo(125,125)
-  //     ctx.lineTo(90,45)
-  //     ctx.lineTo(45,125)
-  //     ctx.closePath()
-  //     ctx.stroke()
+
   function drawPins(){
 
     ctx.fillStyle = gradient
@@ -143,7 +166,6 @@ var pin10
       ctx.moveTo(150,40)
       ctx.lineTo(180,80)
       ctx.lineTo(120,80)
-      ctx.lineTo(150,40)
       ctx.stroke()
       ctx.fill()
       ctx.closePath()
@@ -155,7 +177,6 @@ var pin10
       ctx.moveTo(215,40)
       ctx.lineTo(245,80)
       ctx.lineTo(185,80)
-      ctx.lineTo(215,40)
       ctx.stroke()
       ctx.fill()
       ctx.closePath()
@@ -166,7 +187,6 @@ var pin10
     ctx.moveTo(280,40)
     ctx.lineTo(310,80)
     ctx.lineTo(250,80)
-    ctx.lineTo(280,40)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -178,7 +198,6 @@ var pin10
     ctx.moveTo(345,40)
     ctx.lineTo(375,80)
     ctx.lineTo(315,80)
-    ctx.lineTo(345,40)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -190,7 +209,6 @@ var pin10
     ctx.moveTo(180,85)
     ctx.lineTo(210,125)
     ctx.lineTo(150,125)
-    ctx.lineTo(180,85)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -202,7 +220,6 @@ var pin10
     ctx.moveTo(245,85)
     ctx.lineTo(275,125)
     ctx.lineTo(215,125)
-    ctx.lineTo(245,85)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -214,7 +231,6 @@ var pin10
     ctx.moveTo(310,85)
     ctx.lineTo(340,125)
     ctx.lineTo(280,125)
-    ctx.lineTo(310,85)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -226,7 +242,6 @@ var pin10
     ctx.moveTo(210,130)
     ctx.lineTo(240,170)
     ctx.lineTo(180,170)
-    ctx.lineTo(210,130)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -238,7 +253,6 @@ var pin10
     ctx.moveTo(275,130)
     ctx.lineTo(305,170)
     ctx.lineTo(245,170)
-    ctx.lineTo(275,130)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
@@ -251,12 +265,20 @@ var pin10
     ctx.moveTo(245,175)
     ctx.lineTo(275,215)
     ctx.lineTo(215,215)
-    ctx.lineTo(245,175)
     ctx.stroke()
     ctx.fill()
     ctx.closePath()
   }
 }
+
+  function hitPins(){
+    if( pin10 == true && (y-radius < 215) && (x >= 215 && x <= 275)){
+     pin10 = false
+     x = 245
+     y = 500
+     upPressed = false
+    }
+  }
 
   function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -267,9 +289,9 @@ var pin10
     }else if (leftPressed && (x > radius)) {
       x -= 3
     }else if (upPressed) {
-      while(y > 215){
-        y += dy
-      }
+      // setInterval(hitPins, 40)
+      y -= 3
+      hitPins();
     }
 
 }
@@ -297,7 +319,5 @@ var pin10
       rightPressed = false
     }else if (e.keyCode == 37) {
       leftPressed = false
-    }else if (e.keyCode == 38) {
-      upPressed = false
     }
   }
